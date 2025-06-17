a8bf1a8c56d8bb19313f6bcca8430aab92de6d56
John Waters <john.waters@cooptel.net>
2025-06-17 19:09:03 +0100

    Error-handler endpoints and templates updated: app/errors.py app/templates/401.html app/templates/403.html app/templates/404.html app/templates/500.html

--- app/errors.py
+++ app/errors.py
@@ -3,14 +3,29 @@
 from flask import render_template
 from app import app
 
+from app.core.slate_core import get_hub_mode
+from app.core.slate_core import get_version
+
 @app.errorhandler(401)
 def access_unauthorized_error(error):
-    return render_template('401.html'), 401
+    return render_template(
+        "401.html",
+        hub_mode = get_hub_mode(),
+        version = get_version()
+    ), 401
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template(
+        "404.html",
+        hub_mode = get_hub_mode(),
+        version = get_version()
+    ), 404
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template(
+        "500.html",
+        hub_mode = get_hub_mode(),
+        version = get_version()
+    ), 500
